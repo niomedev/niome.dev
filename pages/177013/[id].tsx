c3ec8bb3bd8693a67b3e09cfa3790ce8e6ec8266
Niome <athyst7@gmail.com>
2021-05-02 16:57:58 +0000

    some more fixes

--- pages/177013/[id].tsx
+++ pages/177013/[id].tsx
@@ -45,3 +45,29 @@ export default function Manga() {
         )
     }else return <DefaultErrorPage statusCode={404}/>
 }
+
+export async function getStaticPaths() {
+    // Call an external API endpoint to get posts
+    const res = await fetch('https://.../posts')
+    const posts = await res.json()
+  
+    // Get the paths we want to pre-render based on posts
+    const paths = posts.map((post:any) => ({
+      params: { id: post.id },
+    }))
+  
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+    return { paths, fallback: false }
+  }
+  
+  // This also gets called at build time
+  export async function getStaticProps({ params }:{ params:any }) {
+    // params contains the post `id`.
+    // If the route is like /posts/1, then params.id is 1
+    const res = await fetch(`https://.../posts/${params.id}`)
+    const post = await res.json()
+  
+    // Pass post data to the page via props
+    return { props: { post } }
+  }
