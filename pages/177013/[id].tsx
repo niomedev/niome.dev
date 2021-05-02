3f0c5be597685a7f9fefb4db35a95aca1bb67938
Niome <athyst7@gmail.com>
2021-05-02 16:38:07 +0000

    Old Route

---
+++ pages/177013/[id].tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link'
+import { ImageContainer, Text } from '../../components/Manga/Manga'
+import DefaultErrorPage from 'next/error'
+import Head from 'next/head'
+
+export default function Manga() {
+
+    function addOne(num:number) {
+        return ++num;
+    }
+
+    const router = useRouter()
+    const { id } = router.query;
+    const convId = id as unknown as number;
+
+    const newNum = addOne(convId);
+    let newUrl;
+
+    if(convId > 0 == true && convId < 15 == true){
+        if(convId < 14){
+            newUrl = `/177013/${newNum}`
+        }else{
+            newUrl = `/177013/${id}`
+        }
+        
+        const imgUrl = `/Manga/Ch1/${id}.png`
+    
+        return(
+            <>
+                <Head>
+                    <title>Rie&apos;s Manga</title>
+                    <link rel="icon" href="/Assets/Rie.png" />
+                </Head>
+                <ImageContainer>
+                    <Text>
+                        {id}/14
+                    </Text>
+                    <Link href={newUrl} passHref>
+                        <img src={imgUrl}/>
+                    </Link>
+                </ImageContainer>
+            </>
+        )
+    }else return <DefaultErrorPage statusCode={404}/>
+}
