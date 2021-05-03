766b33418ce81b463074af81eff4cc1a15ba3e49
Niome <athyst7@gmail.com>
2021-05-03 04:27:14 +0000

    fix manga mobile

--- components/Manga/Manga.tsx
+++ components/Manga/Manga.tsx
@@ -1,11 +1,39 @@
 import styled from 'styled-components';
 
 export const ImageContainer = styled.div`
-    background: #160606;
+    background: #161616;
+    display: flex;
+    height: auto;
+    width: 100%;
+    justify-content: center;
+`
+export const Text = styled.h1`
+    color: white;
+    font-size:48px;
+    text-align: center;
+    justify-content: center;
+    position: relative;
+
+    @media screen and (max-width: 768px){
+        font-size: 36px;
+    }
+`
+
+export const Image = styled.img`
+    height: auto;
+    width: 65%;
+
+    @media screen and (max-width: 955px){
+        width: 100%;
+    }
+`
+
+export const ButtonContainer = styled.div`
+    background: #161616;
     display: flex;
     justify-content: center;
 `
-export const Text = styled.div`
-    color: Black;
-    font-size:48px;
+
+export const Background = styled.body`
+    background: #161616;
 `
--- pages/177013/[id].tsx
+++ pages/177013/[id].tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link'
-import { ImageContainer, Text } from '../../components/Manga/Manga'
+import { Image, ImageContainer, ButtonContainer, Background } from '../../components/Manga/Manga'
 import DefaultErrorPage from 'next/error'
 import Head from 'next/head'
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { BtnWrap, Button } from '../../components/InfoSection/InfoElements';
 
 export default function Manga() {
 
     function addOne(num:number) {
         return ++num;
+    }
+    
+    function delOne(num:number) {
+        return --num;
     }
 
     const router = useRouter()
@@ -17,13 +22,22 @@ export default function Manga() {
     const convId = id as unknown as number;
 
     const newNum = addOne(convId);
+    let oldNum = delOne(convId);
+
+    if(oldNum < 1){
+        oldNum = 1;
+    }
+
     let newUrl;
+    let prevUrl;
 
     if(convId > 0 == true && convId < 15 == true){
         if(convId < 14){
-            newUrl = `/177013/${newNum}`
+            newUrl = `/177013/${newNum}`;
+            prevUrl = `/177013/${oldNum}`;
         }else{
-            newUrl = `/177013/${id}`
+            newUrl = `/177013/${id}`;
+            prevUrl = `/177013/${id}`;
         }
         
         const imgUrl = `/Manga/Ch1/${id}.png`
@@ -34,14 +48,24 @@ export default function Manga() {
                     <title>Rie&apos;s Manga</title>
                     <link rel="icon" href="/Assets/Rie.png" />
                 </Head>
+                <Background />
                 <ImageContainer>
-                    <Text>
-                        {id}/14
-                    </Text>
                     <Link href={newUrl} as={newUrl} prefetch passHref>
-                        <img src={imgUrl}/>
+                        <Image src={imgUrl}/>
                     </Link>
                 </ImageContainer>
+                <ButtonContainer>
+                    <BtnWrap btnEnable={true}>
+                            <Link href={prevUrl} as={prevUrl} prefetch passHref>
+                                <Button>&lt; Previous</Button>
+                            </Link>
+                    </BtnWrap>
+                    <BtnWrap btnEnable={true}>
+                            <Link href={newUrl} as={newUrl} prefetch passHref>
+                                <Button>Next &gt;</Button>
+                            </Link>
+                    </BtnWrap>
+                </ButtonContainer>
             </>
         )
     }else return <DefaultErrorPage statusCode={404}/>
